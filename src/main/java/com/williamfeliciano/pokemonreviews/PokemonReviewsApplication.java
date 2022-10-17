fa7719170734dcc8bcfad99e89d2ee29d6303fd7
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-10-16 23:15:45 -0400

    Initial Commit

---
+++ src/main/java/com/williamfeliciano/pokemonreviews/PokemonReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.williamfeliciano.pokemonreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/williamfeliciano/pokemonreviews/PokemonReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamfeliciano.pokemonreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

709a6627c11a94db61fdc9c94cc20e17d9275f47
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-10-22 20:22:30 -0400

    Added Entity Tables

--- src/main/java/com/williamfeliciano/pokemonreviews/models/Pokemon.java
+++ src/main/java/com/williamfeliciano/pokemonreviews/models/Pokemon.java
@@ -4,11 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Pokemon {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String type;
--- src/main/java/com/williamfeliciano/pokemonreviews/models/Review.java
+++ src/main/java/com/williamfeliciano/pokemonreviews/models/Review.java
@@ -4,11 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Review {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String content;

fee9cb3530e0baef7e693e42cb290c59c6c925e2
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-10-23 21:10:12 -0400

    Initial PokemonController CRUD Functionality

--- src/main/java/com/williamfeliciano/pokemonreviews/controllers/PokemonController.java
+++ src/main/java/com/williamfeliciano/pokemonreviews/controllers/PokemonController.java
@@ -1,10 +1,9 @@
 package com.williamfeliciano.pokemonreviews.controllers;
 
 import com.williamfeliciano.pokemonreviews.models.Pokemon;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,4 +20,40 @@ public class PokemonController {
         pokemons.add(new Pokemon(7,"charmander","fire"));
         return ResponseEntity.ok().body(pokemons);
     }
+
+    @GetMapping("pokemon/{id}")
+    public ResponseEntity<Pokemon> getSpecificPokemon(@PathVariable("id") int id){
+        List<Pokemon> pokemons = new ArrayList<>();
+        pokemons.add(new Pokemon(1,"bulbasaur","grass"));
+        pokemons.add(new Pokemon(4,"squirtle","water"));
+        pokemons.add(new Pokemon(7,"charmander","fire"));
+
+        Pokemon pokemonToReturn = pokemons.stream().filter(pokemon -> pokemon.getId()== id).findFirst().get();
+        return ResponseEntity.ok().body(pokemonToReturn);
+    }
+
+    @PostMapping("pokemon/create")
+    public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon){
+        return new ResponseEntity<>(pokemon,HttpStatus.CREATED);
+    }
+
+    @PutMapping("pokemon/{id}/update")
+    public ResponseEntity<Pokemon> updatePokemon(@PathVariable("id") int id, @RequestBody Pokemon updatedPokemon){
+        List<Pokemon> pokemons = new ArrayList<>();
+        pokemons.add(new Pokemon(1,"bulbasaur","grass"));
+        pokemons.add(new Pokemon(4,"squirtle","water"));
+        pokemons.add(new Pokemon(7,"charmander","fire"));
+
+        Pokemon pokemonToReturn = pokemons.stream().filter(pokemon -> pokemon.getId()== id).findFirst().get();
+        pokemonToReturn.setName(updatedPokemon.getName());
+        pokemonToReturn.setType(updatedPokemon.getType());
+        return ResponseEntity.ok().body(pokemonToReturn);
+    }
+
+    @DeleteMapping("pokemon/{id}/delete")
+    public ResponseEntity<?> deletePokemon(@PathVariable("id") int id){
+        System.out.println(id);
+        return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
+    }
+
 }
